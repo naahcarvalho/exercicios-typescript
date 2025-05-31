3d370f539c192832fcbaec36656acffe1c4b7469
Naah Carvalho <naah_.carvalho@outlook.com>
2025-05-30 22:11:22 -0300

    ajustes e melhorias nos exercícios

--- collections/set.ts
+++ collections/set.ts
@@ -3,11 +3,11 @@ import leia from "readline-sync";
 const lista: Set<number> = new Set<number>();
 const repetidos: Set<number> = new Set<number>();
 
-while (lista.size < 10) {
+for (let contador = 0; contador < 10; contador++) {
     let numero = leia.questionInt("Digite um numero: ");
 
     if (lista.has(numero)) {
-        repetidos.add(numero);
+        repetidos.add(numero);  
     } else {
         lista.add(numero);
     }
@@ -17,18 +17,20 @@ function pularLinha() {
     console.log();
 }
 
-pularLinha()
+pularLinha();
 
-console.log("Números sem repeticao:");
-for (let numero of lista) {
+const listaOrdenada = Array.from(lista).sort((a, b) => a - b);
+console.log("Numeros digitados:");
+for (let numero of listaOrdenada) {
     console.log(numero);
 }
 
-pularLinha()
+pularLinha();
 
 if (repetidos.size > 0) {
-    console.log("Numeros repetidos digitados: ");
-    for (let num of repetidos) {
+    const repetidosOrdenados = Array.from(repetidos).sort((a, b) => a - b);
+    console.log("Numeros repetidos:");
+    for (let num of repetidosOrdenados) {
         console.log(num);
     }
 } else {
